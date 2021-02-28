77bc8791668d38e20dfbd6cee58ec65403a1b01e
Shreyash <shreyashnkadam2000@gmail.com>
2021-02-28 10:42:51 +0530

    searchbar

--- channel.php
+++ channel.php
@@ -87,9 +87,15 @@
     <div class="container-fluid">
      <div class="row">
          <div class="col-sm-4">
-             search notes,
-             event list,
-             doubts
+             <div class="input-group">
+  <div class="form-outline">
+    <input type="search" id="form1" class="form-control" />
+    <label class="form-label" for="form1">Search</label>
+  </div>
+  <button type="button" class="btn btn-primary">
+    <i class="fas fa-search"></i>
+  </button>
+</div>
          </div>
          <div class="col-sm-8"> 
              
